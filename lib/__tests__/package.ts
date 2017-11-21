1ccd274ee399b3c15418ceeb78b2fbe637e05790
uhyo <uhyo@uhy.ooo>
2017-11-22 01:59:34 +0900

    just test for package.ts

---
+++ lib/__tests__/package.ts
@@ -0,0 +1,58 @@
+// Tests for Package functionality.
+import {
+    Game,
+} from '../game';
+import {
+    makePackage,
+    Package,
+} from '../package';
+
+describe('Package', ()=> {
+    it('remembers its name', ()=> {
+        class SampleModule {}
+        const p = new Package('test.module', SampleModule);
+
+        expect(p.name).toBe('test.module');
+    });
+    it('initializes given module when make', ()=> {
+        const cl = jest.fn();
+
+        const p = new Package('test.module', cl);
+        const game = new Game();
+
+        expect(cl).not.toHaveBeenCalled();
+
+        const res = p.make(game);
+        // constructor should be called once
+        expect(cl).toHaveBeenCalledTimes(1);
+        // Result of make should be the result of new
+        expect(res).toBe(cl.mock.instances[0]);
+    });
+    it('stores module for each game', ()=> {
+        const cl = jest.fn();
+
+        const p = new Package('test.module', cl);
+        const game1 = new Game();
+        const game2 = new Game();
+
+        const res = p.make(game1);
+
+        // Package should remember a module for game1
+        expect(p.get(game1)).toBe(res);
+
+        // game2 is not initialized yet
+        expect(p.get(game2)).toBe(null);
+    });
+});
+describe('makePackage', ()=>{
+    it('initializes a package with given name', ()=> {
+        const cl = jest.fn();
+        const p = makePackage('test.module')(cl);
+
+        expect(p).toBeInstanceOf(Package);
+        expect(p.name).toBe('test.module');
+
+        const res = p.make(new Game());
+        expect(res).toBe(cl.mock.instances[0]);
+    });
+});
